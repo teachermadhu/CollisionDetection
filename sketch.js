106606d17171437822aebadb093d3fddc094889f
teachermadhu <64385453+teachermadhu@users.noreply.github.com>
2020-04-30 14:44:21 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,28 +1,51 @@
 var fixedRect,movingRect;
+var gameObject1,gameObject2,gameObject3,gameObject4;
 function setup() {
   createCanvas(800,400);
   fixedRect = createSprite(400, 200, 50, 80);
   fixedRect.shapeColor="green";
-  fixedRect.debug=true;
+
   movingRect = createSprite(400,200,80,30);
   movingRect.shapeColor="green";
-  movingRect.debug=true;
+
+  gameObject1 = createSprite(100,100,50,50);
+  gameObject1.shapeColor="green";
+
+  gameObject2 = createSprite(200,100,50,50);
+  gameObject2.shapeColor="green";
+
+  gameObject3 = createSprite(300,100,50,50);
+  gameObject3.shapeColor="green";
+
+  gameObject4 = createSprite(400,100,50,50);
+  gameObject4.shapeColor="green";
 }
 
 function draw() {
   background(0,0,0);  
   movingRect.x = World.mouseX;
   movingRect.y = World.mouseY;
-  if(movingRect.x - fixedRect.x < fixedRect.width/2 + movingRect.width/2 
-  && fixedRect.x - movingRect.x < fixedRect.width/2 + movingRect.width/2
-  && movingRect.y - fixedRect.y < fixedRect.height/2 + movingRect.height/2 
-  && fixedRect.y - movingRect.y < fixedRect.height/2 + movingRect.height/2){
-    movingRect.shapeColor="red";
-    fixedRect.shapeColor="red";
-  }
-  else{
-    movingRect.shapeColor="green";
-    fixedRect.shapeColor="green";
-  }
+  console.log(movingRect.x - fixedRect.x);
+ if(isTouching(movingRect,gameObject1)){
+  movingRect.shapeColor="blue";
+  fixedRect.shapeColor="blue";
+ }else{
+  movingRect.shapeColor="green";
+  fixedRect.shapeColor="green";
+ }
+
+ 
   drawSprites();
+}
+
+function isTouching(object1,object2){
+  if(object1.x - object2.x < object2.width/2 + object1.width/2 
+    && object2.x - object1.x < object2.width/2 + object1.width/2
+    && object1.y - object2.y < object2.height/2 + object1.height/2 
+    && object2.y - object1.y < object2.height/2 + object2.height/2){
+      return true;
+    }
+    else{
+     return false;
+    }
 }

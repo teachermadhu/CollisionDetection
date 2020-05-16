b4ffe9e8296a7de069126bb73fa528fdb6473e63
teachermadhu <64385453+teachermadhu@users.noreply.github.com>
2020-05-16 17:26:33 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -26,11 +26,11 @@ function draw() {
   movingRect.x = World.mouseX;
   movingRect.y = World.mouseY;
   console.log(movingRect.x - fixedRect.x);
-  if(movingRect.x - fixedRect.x < fixedRect.width/2 + movingRect.width/2 
+  if (movingRect.x - fixedRect.x < fixedRect.width/2 + movingRect.width/2
     && fixedRect.x - movingRect.x < fixedRect.width/2 + movingRect.width/2
-    && movingRect.y - fixedRect.y < fixedRect.height/2 + movingRect.height/2 
-    && fixedRect.y - movingRect.y < fixedRect.height/2 + movingRect.height/2){
-  movingRect.shapeColor="blue";
+    && movingRect.y - fixedRect.y < fixedRect.height/2 + movingRect.height/2
+    && fixedRect.y - movingRect.y < fixedRect.height/2 + movingRect.height/2) {
+		movingRect.shapeColor="blue";
   fixedRect.shapeColor="blue";
  }else{
   movingRect.shapeColor="green";
